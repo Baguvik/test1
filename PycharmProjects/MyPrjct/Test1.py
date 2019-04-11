1324580c0d7afce26c1600ea53ece6767d0b7bfa
Baguvik <makdvim@gmail.com>
2019-04-11 19:19:10 -0400

    homework commit

---
+++ PycharmProjects/MyPrjct/Test1.py
@@ -0,0 +1,10 @@
+Number=1.34e-4
+print(Number**2)
+str='rast'
+print (len(str))
+list=[1,[2,'three'],4]
+print(list[1])
+tupl=(1,'saq',4,5)
+print(tupl[1])
+x=set('quickbrownfoxjumpoverthelazydog')
+print(x)
